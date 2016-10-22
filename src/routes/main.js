cc195a2e5c79c31b36ceb0ce1075920e2a0eeec0
ChadoNihi <chadoishere@gmail.com>
2016-10-22 10:15:48 +0300

    laptop is dying

---
+++ src/routes/main.js
@@ -0,0 +1,3 @@
+
+
+export {showMainPage, returnBySearch, returnLatest};
--- src/server.js
+++ src/server.js
@@ -1,11 +1,28 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import {showMainPage} from './routes';
+import stylus from 'stylus';
+import nib from 'nib';
+
 const app = express(),
-      port = 3000;
+      port = process.env.PORT || 5000,
+      srchEngApi = 'https://www.googleapis.com/customsearch/v1?q=nuts&cx=013008978640622301934%3Ayuqjeg-j9km&searchType=image&key='+process.env.SEARCH_KEY,
+      compile = (str, path)=> {
+          return stylus(str)
+              .set('filename', path)
+              .use(nib());
+      };
 
-app.get('/', (req,res)=>{
-    res.send('Running');
-});
+dotenv.config();
+app.set( 'port', ( process.env.PORT || 5000 ));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'pug');
 
-app.listen(port, 'localhost', ()=>{
-    console.log(`The server is listening to port ${port}`);
+
+app.get('/', showMainPage);
+app.get('/img-srch', returnBySearch);
+app.get('/img-srch/latest', returnLatest);
+
+app.listen(app.get('port'), 'localhost', ()=>{
+    console.log(`The server is listening to port ${app.get('port')}`);
 });
